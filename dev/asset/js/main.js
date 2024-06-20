306de0a66f28f94a5644d5b0ba4484a8dbedbc36
leehanbyeul <as352@naver.com>
2024-06-20 15:05:40 +0900

    mainvisual 스와이퍼

---
+++ dev/asset/js/main.js
@@ -0,0 +1,18 @@
+//Swiper slider
+var swiper = new Swiper(".main-visual .swiper", {
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    }
+);
